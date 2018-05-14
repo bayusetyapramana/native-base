623626055cc6ca17c17d9fdfa852672d30641942
bayusetyapramana <bayusetyapramana@gmail.com>
2018-05-14 11:22:48 +0700

    add header and body

--- react-native-apps/NotToDoList/App.js
+++ react-native-apps/NotToDoList/App.js
@@ -5,10 +5,24 @@ export default class  App extends Component {
   render(){
     return (
       <Container>
+
+        <Header>
+          <Body>
+            <Text>Not To Do List</Text>
+          </Body>
+        </Header>
         <Content>
+          <div style={styles.container}>
           <Text> Hello Native Base!</Text>
+          </div>
         </Content>
       </Container>
     );
   }
 }
+
+const styles = StyleSheet.create({
+container: {
+  flex: 1
+},
+})
